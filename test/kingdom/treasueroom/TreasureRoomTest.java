02b31c7a649f8431a4ce316adc49f2862f8d155a
273962 <48473715+273962@users.noreply.github.com>
2019-11-24 10:43:40 +0100

    minor adjustment

---
+++ test/kingdom/treasueroom/TreasureRoomTest.java
@@ -0,0 +1,30 @@
+package kingdom.treasueroom;
+
+import kingdom.valuables.Materials;
+import kingdom.valuables.Valuable;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TreasureRoomTest {
+    TreasureRoom treasureRoom;
+    Valuable valuable;
+
+
+
+    @BeforeEach
+    void setUp() {
+        treasureRoom = new TreasureRoom();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void addOneValuableToTreasureRoom(){
+
+    }
+}
